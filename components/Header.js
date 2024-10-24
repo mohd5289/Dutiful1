1738e2f22444beecb9c05a383bf6a27826898887
Osuolale <mohd@yahoo.com>
2024-10-24 21:24:13 +0100

    Completed responsiveness

--- components/Header.js
+++ components/Header.js
@@ -75,10 +75,10 @@ const Header = () => {
             </div>
           </div>
           <div className="flex gap-2 font-circular ">
-            <button className="bg-transparent text-[#1E1E4B] hover:text-blue-700 font-semibold py-2 px-4 min-w-16 text-sm lg:text-base">
+            <button className="bg-transparent text-[#1E1E4B] hover:text-blue-700 font-semibold py-1 px:1 md:py-2 md:px-4 min-w-16 text-sm lg:text-base">
               Login
             </button>
-            <button className="bg-transparent text-[#603F8B] border-2 border-[#603F8B] border-solid hover:bg-[#603F8B] hover:text-white font-semibold py-2 px-4 rounded-md min-w-16 text-sm lg:text-base">
+            <button className="bg-transparent text-[#603F8B] border-2 border-[#603F8B] border-solid hover:bg-[#603F8B] hover:text-white font-semibold py-1 px:1 md:py-2 md:px-4 rounded-md min-w-16 text-sm lg:text-base">
               Sign Up
             </button>
           </div>
